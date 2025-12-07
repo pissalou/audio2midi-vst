fe0715270e0a7d2b3561258cf8fa00ffbdf0d853
pissalou <pascal.mazars@gmail.com>
2025-12-07 00:01:01 +0000

    fix: ensure submodules are fetched in post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,5 +2,8 @@
 
 set -euxo pipefail
 
+# Fetch submodules (JUCE, metatonin...)
+git submodule update --init --recursive
+
 # Configure the build system (cmake) right from the start.
 cmake . -B Builds -G Ninja -DCMAKE_BUILD_TYPE=Release
